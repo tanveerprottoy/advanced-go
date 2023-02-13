38e9097df1887534152b8b9963f57cf059168494
tanveerprottoy <tanveer.prottoy@gmail.com>
2023-02-13 17:25:09 +0600

    add more code

--- async/async.go
+++ async/async.go
@@ -8,12 +8,17 @@ import (
 func SleeperFunc(ch chan int, d time.Duration) {
 	fmt.Println("sleeping for: ", d)
 	time.Sleep(d)
+	// send back the channel with value
 	ch <- 0
 }
 
 func CallerFunc() {
 	ch := make(chan int)
+	// want to wait and get some value
+	// from the SleeperFunc goroutine
+	// for that sending the channel
 	go SleeperFunc(ch, time.Second*5)
+	// receive the value from channel
 	res := <-ch
 	fmt.Println(res)
 }

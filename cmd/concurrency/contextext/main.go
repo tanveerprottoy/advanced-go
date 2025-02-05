506af5bfa7345891390250ef79eaadf59b7626a8
tanveerprottoy <tanveer.prottoy@gmail.com>
2025-02-05 18:18:08 +0600

    add context cancallecation

---
+++ cmd/concurrency/contextext/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/tanveerprottoy/advanced-go/concurrency/contextext"
+)
+
+func main() {
+	contextext.ExecuterEstablishNetConnection()
+}
---
+++ concurrency/contextext/context.go
@@ -0,0 +1,56 @@
+package contextext
+
+import (
+	"context"
+	"log"
+	"math/rand"
+	"time"
+)
+
+// establishNetworkConnection simulates a long running network connection
+// a write only channel is passed to the function
+func establishNetworkConnection(ch chan<- bool) {
+	log.Println("Establishing network connection...")
+
+	// simulate success and failure
+	// based on random number
+	if rand.Intn(10) < 5 {
+		time.Sleep(2 * time.Second)
+
+		log.Println("Network connection established")
+	} else {
+		log.Println("Failed to establish network connection")
+
+		// simulate hanging forever
+		time.Sleep(10 * time.Minute)
+	}
+
+	// write/send value to the channel
+	ch <- true
+}
+
+func establishNetworkConnectionWithTimeout() {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+
+	defer cancel()
+
+	// create a channel to receive the result
+	// it's an unbuffered channel
+	ch := make(chan bool)
+
+	// start the goroutine
+	go establishNetworkConnection(ch)
+
+	// wait on multiple channels
+	select {
+	case <-ch:
+		log.Println("Network connection established")
+	case <-ctx.Done():
+		log.Println("Timeout. Cancelling network connection")
+	}
+
+}
+
+func ExecuterEstablishNetConnection() {
+	establishNetworkConnectionWithTimeout()
+}

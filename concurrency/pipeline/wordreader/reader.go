d89406f3d5af9a1ee7b2c58d06df07be2b05aa0a
tanveerprottoy <tanveer.prottoy@gmail.com>
2025-03-12 23:24:57 +0600

    add workerpool

---
+++ concurrency/pipeline/wordreader/reader.go
@@ -0,0 +1,170 @@
+package wordreader
+
+import (
+	"context"
+	"errors"
+	"log"
+	"strings"
+	"sync"
+	"time"
+
+	"golang.org/x/sync/semaphore"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+func producer(ctx context.Context, strings []string) (<-chan string, error) {
+	outChannel := make(chan string)
+
+	go func() {
+		defer close(outChannel)
+
+		for _, s := range strings {
+			select {
+			case <-ctx.Done():
+				return
+			case outChannel <- s:
+			}
+		}
+	}()
+
+	return outChannel, nil
+}
+
+func sink(ctx context.Context, cancelFunc context.CancelFunc, values <-chan string, errors <-chan error) {
+	for {
+		select {
+		case <-ctx.Done():
+			log.Print(ctx.Err().Error())
+			return
+		case err := <-errors:
+			if err != nil {
+				log.Println("error: ", err.Error())
+				cancelFunc()
+			}
+		case val, ok := <-values:
+			if ok {
+				log.Printf("sink: %s", val)
+			} else {
+				log.Print("done")
+				return
+			}
+		}
+	}
+}
+
+func step[In, Out any](
+	ctx context.Context,
+	inputChannel <-chan In,
+	fn func(In) (Out, error),
+) (chan Out, chan error) {
+	outputChannel := make(chan Out)
+	errorChannel := make(chan error)
+
+	limit := int64(2)
+	// Use all CPU cores to maximize efficiency. We'll set the limit to 2 so you
+	// can see the values being processed in batches of 2 at a time, in parallel
+	// limit := int64(runtime.NumCPU())
+	sem1 := semaphore.NewWeighted(limit)
+
+	go func() {
+		defer close(outputChannel)
+		defer close(errorChannel)
+
+	Outerloop:
+		for {
+			select {
+			case <-ctx.Done():
+				break Outerloop
+			case s, ok := <-inputChannel:
+				if ok {
+					if err := sem1.Acquire(ctx, 1); err != nil {
+						log.Printf("Failed to acquire semaphore: %v", err)
+						break
+					}
+
+					go func(s In) {
+						defer sem1.Release(1)
+						time.Sleep(time.Second * 3)
+
+						result, err := fn(s)
+						if err != nil {
+							errorChannel <- err
+						} else {
+							outputChannel <- result
+						}
+					}(s)
+				} else {
+					if err := sem1.Acquire(ctx, limit); err != nil {
+						log.Printf("Failed to acquire semaphore: %v", err)
+					}
+
+					return
+				}
+			}
+		}
+	}()
+
+	return outputChannel, errorChannel
+}
+
+func Merge[T any](ctx context.Context, cs ...<-chan T) <-chan T {
+	var wg sync.WaitGroup
+	out := make(chan T)
+
+	output := func(c <-chan T) {
+		defer wg.Done()
+		for n := range c {
+			select {
+			case out <- n:
+			case <-ctx.Done():
+				return
+			}
+		}
+	}
+
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
+}
+
+func transformA(s string) (string, error) {
+	log.Println("transformA input: ", s)
+	return strings.ToLower(s), nil
+}
+
+func transformB(s string) (string, error) {
+	log.Println("transformB input: ", s)
+	// Comment this out to see the pipeline finish successfully
+	if s == "foo" {
+		return "", errors.New("oh no")
+	}
+
+	return cases.Title(language.English).String(s), nil
+}
+
+func Executer() {
+	source := []string{"FOO", "BAR", "Foobar"}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	readStream, err := producer(ctx, source)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	step1results, step1errors := step(ctx, readStream, transformA)
+	step2results, step2errors := step(ctx, step1results, transformB)
+	allErrors := Merge(ctx, step1errors, step2errors)
+
+	sink(ctx, cancel, step2results, allErrors)
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22
 
 require (
 	github.com/mmcdole/gofeed v1.3.0
+	golang.org/x/sync v0.11.0
 	golang.org/x/text v0.22.0
 )
 
